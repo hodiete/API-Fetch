a2c784610cad8272a000c9f64a930b7d7bcb4e1f
Henry Odiete <henry.odiete@opin.ca>
2019-09-23 01:43:47 -0400

    made changes to the fetch data using axios

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-var express= require('express'),
-    app= express(), 
-    ejs = require('ejs'),
-    request = require('request'),
-    worker_orders,
-    workers=[],
-    sorter=[];
+var express = require("express"),
+  app = express(),
+  axios = require("axios"),
+  worker_orders = [],
+  sorter = [],
+  WorkerWithOrders = [];
 
 app.set("view engine", "ejs");
-app.use (express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
+fetchWorkOrdersApi()
+  .then(data => data.data.orders)
+  .then(order => {
+    worker_orders = order;
+    return order.map(ord => ord.workerId);
+  })
+  .then(uniqueOrderID => Array.from(new Set(uniqueOrderID)))
+  .then(orderID =>
+    fetchWorkersApi(orderID).then(
+      data => (workers = data.map(order => order.data.worker))
+    )
+  )
+  .then(orders =>
+    worker_orders.forEach(workOrder => {
+      WorkerWithOrders.push([getWorkerInfo(workOrder, orders), workOrder]);
+      sorter.push(workOrder.deadline);
+    })
+  );
 
-request("https://www.hatchways.io/api/assessment/work_orders", function (error, response, body){
-    if (!error && response.statusCode==200){
-    	worker_orders= JSON.parse(body);
-       worker_orders['orders'].forEach(function(order){
-       	      request("https://www.hatchways.io/api/assessment/workers/"+order.workerId, function( err, response, body){
-               if (!error && response.statusCode==200){
-               var worker= JSON.parse(body);
-                  workers.push([worker.worker, order]);  
-                  sorter.push(order.deadline); 
-              }else{
-              	console.log(err);
-              }
-       	      });     
-     });
-    }else{
-    	console.log(err);
-    }
+async function fetchWorkOrdersApi() {
+  const work = await axios.get(
+    "https://www.hatchways.io/api/assessment/work_orders"
+  );
+  return work;
+}
+let getWorkerInfo = (order, workers) =>
+  workers.find(worker => worker.id === order.workerId);
 
-});
+function fetchWorkersApi(orderID) {
+  const p = orderID.map(
+    async orders =>
+      await axios.get(
+        "https://www.hatchways.io/api/assessment/workers/" + orders
+      )
+  );
+
+  return Promise.all(p);
+}
 
 //ROUTES
-app.get('/', function (req, res){	  
-	 res.render('home', {worker:workers, sorter:sorter});
+app.get("/", function(req, res) {
+  res.render("home", { worker: WorkerWithOrders, sorter: sorter });
 });
 
-app.listen (3000, function(req, res){
-	console.log("The challenge is running!");
+app.listen(3001, function(req, res) {
+  console.log("The challenge is running!");
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,46 +1,42 @@
-$('.asc').show();
-$('.desc').hide();
+$(".asc").show();
+$(".desc").hide();
 
-$('#desc').on('click', function(){
-  $('.asc').hide();
-  $('.desc').show();     
-  $('input[type="checkbox"]').prop('checked', false);
+$("#desc").on("click", function() {
+  $(".asc").hide();
+  $(".desc").show();
+  $('input[type="checkbox"]').prop("checked", false);
 });
 
-$('#asc').on('click', function(){
-    $('.desc').hide();
-    $('.asc').show();
-    $('input[type="checkbox"]').prop('checked', true);
+$("#asc").on("click", function() {
+  $(".desc").hide();
+  $(".asc").show();
+  $('input[type="checkbox"]').prop("checked", true);
 });
 
-$('input[type="checkbox"]').click(function(){
-  if($(this).prop("checked") == true){
-    $('.desc').hide();
-    $('.asc').show();
-  } else if($(this).prop("checked") == false){
-    $('.asc').hide();
-   	$('.desc').show();
-  
+$('input[type="checkbox"]').click(function() {
+  if ($(this).prop("checked") == true) {
+    $(".desc").hide();
+    $(".asc").show();
+  } else if ($(this).prop("checked") == false) {
+    $(".asc").hide();
+    $(".desc").show();
   }
 });
 
+$('input[type="search"]').on("keyup", function() {
+  var searchValue = $(this)
+      .val()
+      .toLowerCase(),
+    names = document.querySelectorAll(".info span"),
+    nameDisplay = document.querySelectorAll(".container");
 
-$('input[type="search"]').on('keyup', function(){
-  var searchValue= $(this).val().toLowerCase(),
-      names= document.querySelectorAll('.info span'),
-      nameDisplay= document.querySelectorAll('.container');
-
-  if (searchValue!=" "){
-    names.forEach(function(name,i){
-    if ( name.innerHTML.toLowerCase().indexOf(searchValue) > -1) {
+  if (searchValue != " ") {
+    names.forEach(function(name, i) {
+      if (name.innerHTML.toLowerCase().indexOf(searchValue) > -1) {
         nameDisplay[i].style.display = "";
-    } else {
+      } else {
         nameDisplay[i].style.display = "none";
-    }
+      }
     });
-  }   
-    
+  }
 });
-
-
-
